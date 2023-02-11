bc5f161a44167f339c56dab85d4464aba449a266
rueby <ruthebubechi@gmail.com>
2023-02-11 02:11:23 +0100

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>14th Febuary, 2023</div>
+      <div>
+        <h2>Car fixing</h2>
+        <div> $2000</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
